8d9017cc7e46c59121e14cf3be4f73c9f48b68e1
fezita <78113405+fezita@users.noreply.github.com>
2021-06-16 19:08:31 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from "react";
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container"> 
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"/>
       </header>
+      <main>
+        <Dictionary /> 
+      </main>
+      <footer className="App-footer">
+        <small> 
+        Coded by Fernanda Schmitt
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
