9fec6ab5cb27aa064b5582d6b87384151e0ad162
fezita <78113405+fezita@users.noreply.github.com>
2021-06-17 17:28:29 +0100

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import './Dictionary.css';
+import axios from "axios";
+
+//https://api.dictionaryapi.dev/api/v2/entries/en_GB/hello
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response);
+    }
+
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event){
         setKeyword(event.target.value);
