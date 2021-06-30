dda7ff4536e3b9d17732d43257c711ddeda0810b
fezita <78113405+fezita@users.noreply.github.com>
2021-06-30 18:14:05 +0100

    Added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,32 @@
 import React, { useState } from 'react';
 import './Dictionary.css';
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handlePexelsResponse(response) {      
-        console.log(response);
+    function handleDictionaryResponse(response) {
+        setResults(response.data[0]);
     }
 
-    function handleResponse(response) {
-        setResults(response.data[0]);
+    function handlePexelsResponse(response) {
+        console.log(response.data);
+        setPhotos(response.data.photos);
     }
 
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handlePexelsResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
 
         let pexelsApiKey = "563492ad6f917000010000012b587041eab54d46bbd0e936b3a24cba";
-        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1"`;
-        axios
-        .get(pexelsApiKey, { 
-            headers: {Authorization: `Bearer ${pexelsApiKey} ` },
-        })
-        .then(handlePexelsResponse);
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = {"Authorization" : `Bearer ${pexelsApiKey}`}
+        axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
     }
 
     function handleSubmit(event){
@@ -59,6 +59,7 @@ export default function Dictionary(props) {
                 </div>
             </section>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     );
     } else {
